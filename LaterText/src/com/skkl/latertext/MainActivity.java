71f2c4788afebc3a72af2ba41c010ea6322961b2
Si Wan Kim <siwan.kim@utexas.edu>
2013-11-22 02:07:51 -0600

    Started to implement sending sms

--- LaterText/src/com/skkl/latertext/MainActivity.java
+++ LaterText/src/com/skkl/latertext/MainActivity.java
@@ -1,15 +1,49 @@
 package com.skkl.latertext;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.provider.ContactsContract;
 
 public class MainActivity extends Activity {
+	
+	EditText recipient, message;
+	TimePicker time;
+	Button submit, contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_text);
+        
+        recipient = (EditText)findViewById(R.id.recipient);
+        message = (EditText)findViewById(R.id.message);
+        time = (TimePicker)findViewById(R.id.time);
+        submit = (Button)findViewById(R.id.submit);
+        contacts = (Button)findViewById(R.id.contacts);
+        
+        submit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// send message to BroadCastReceiver
+				
+			}
+		});
+        
+        contacts.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// Android query for contacts
+				
+			}
+		});
     }
 
 
